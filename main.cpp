158d29d5321dec5dd8ac22db32a3e76cf7ca3802
Megan Greensmith <megangreensmith@MacBook-Pro-6.local>
2019-10-12 12:10:42 -0400

    updated intro

--- main.cpp
+++ main.cpp
@@ -22,10 +22,12 @@ int main()
 
 void intro()
 {
-    cout << "Welcome to Idiot University, a game where how cunning you are determines your place.\n";
-    cout << "You are an 18 year old freshman, who was valedictorian of your senior class.\n";
-    cout << "Now that you have been eccepted into the prestigious Idiot University, you have to prove your worth.\n";
-    cout << "Little did you know,. everyone around you, just might end up being a moron. Despite their academic achievements...\n\n";
+    cout << "Welcome to Into The Deep, a game where your instincts and quick thinking determines whether you live or die.\n";
+    cout << "You are part of an elite group of marine biologists, on a secret mission to discover a new type of oxygen-emitting algae.\n";
+    cout << "This morning you headed down to the sea floor to collect more samples...\n";
+    cout << "But while you were out your underwater lab exploded!\n";
+    cout << "As the sole survivor it is up to you to reach the surface, battling sea monsters along the way";
+    cout << "Just make sure you don't run out of oxygen...\n\n";
 }
 
 void scene1()
