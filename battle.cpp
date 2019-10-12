9b9bba48799be8e252a40333b9869d25590d1e19
hawkeyeshi <hawkeyeshi@gmail.com>
2019-10-12 18:38:34 -0400

    idk

--- battle.cpp
+++ battle.cpp
@@ -15,51 +15,82 @@ void battle()
     scene shrimp("Oh no! You've come into contact with a feisty shrimp! Fortunately it's not very strong and you can easly overcome it", "Shrimp", 5, 2, 1, 3);
     enemy.push_back(shrimp);
     doBattle(user, enemy, index); index++;
+    bubble(user);
+    
     scene goldfish("Here comes an angry goldfish...", "Goldfish", 7, 3, 1, 5);
     enemy.push_back(goldfish);
     doBattle(user, enemy, index); index++;
+    bubble(user);
+    
     scene watersnake("This watersnake is even more agressive than the others. Choose your weapons wisely!", "Watersnake", 9, 4, 3, 7);
     enemy.push_back(watersnake);
     doBattle(user, enemy, index); index++;
+    bubble(user);
+    
     scene seaUrchin("You've uspet a sea urchin! Watch out for that attack level!", "Sea Urchin", 11, 5, 3, 9);
     enemy.push_back(seaUrchin);
     doBattle(user, enemy, index); index++;
+    bubble(user);
+    
     scene pufferFish("Now you've come face-to-face with a puffer fish!", "Puffer Fish", 13, 6, 5, 11);
     enemy.push_back(pufferFish);
     doBattle(user, enemy, index); index++;
+    bubble(user);
+    
     scene crab("Uh oh... this crab does not look happy at all", "Crab", 15, 7, 5, 12);
     enemy.push_back(crab);
     doBattle(user, enemy, index); index++;
+    bubble(user);
+    
     scene turtle("Watch out! This turtle definitley has something up his shell", "Turtle", 17, 8, 5, 15);
     enemy.push_back(turtle);
     doBattle(user, enemy, index); index++;
+    bubble(user);
+    
     scene dolphin("Here comes a vengeful dolphin!", "Dolphin", 19, 9, 6, 15);
     enemy.push_back(dolphin);
     doBattle(user, enemy, index); index++;
+    bubble(user);
+    
     scene stingray("You've now made contact with an octopus! Better keep your wits about you... ", "Stingray", 21, 10, 6, 17);
     enemy.push_back(stingray);
     doBattle(user, enemy, index); index++;
+    bubble(user);
+    
     scene octopus("This octopus is on a mission, but look at that health gain!", "Octopus", 23, 11, 7, 22);
     enemy.push_back(octopus);
     doBattle(user, enemy, index); index++;
+    bubble(user);
+    
     scene seaLion("Here comes one ruthless sea lion", "Sea Lion", 25, 12, 9, 22);
     enemy.push_back(seaLion);
     doBattle(user, enemy, index); index++;
+    bubble(user);
+    
     scene crocodile("This crocodile will certainly not be easy to kill!", "Crocodile", 27, 13, 9, 24);
     enemy.push_back(crocodile);
     doBattle(user, enemy, index); index++;
+    bubble(user);
+    
     scene shark("Beware! It will take a lot to kill this shark", "Shark", 29, 14, 12, 25);
     enemy.push_back(shark);
     doBattle(user, enemy, index); index++;
+    bubble(user);
+    
     scene giantSquid("Almost finished... so get rid of the squid!", "Giant Squid", 31, 15, 13, 26);
     enemy.push_back(giantSquid);
     doBattle(user, enemy, index); index++;
+    bubble(user);
+    
     scene killerWhale("Better kill this whale before it kills you!", "Killer Whale", 33, 16, 16, 29);
     enemy.push_back(killerWhale);
     doBattle(user, enemy, index); index++;
+    bubble(user);
+    
     scene lockNessMonster("THE ULTIMATE CHALLENGE: Defeat this beast and reach the surface!", "Loch Ness Monster", 35, 17, 17, 30);
     enemy.push_back(lockNessMonster);
     doBattle(user, enemy, index); index++;
+    bubble(user);
 
 
 }
@@ -135,19 +166,34 @@ void bubble(player::player user) {
 
 }
 
-    
+void upgradeDistance(player user) {
+    cout << "Current distance traveled per round: " << user.getMetersToTravel << endl;
+    cout << "Would you like to use oxygen to increase this distance? (Y/N)\n";
 
-    /*
-        upgrade strength?
-            oxygen --;
-            strenght += notpop;
+    string response;
+    cin >> response;
 
-        if they pop
-            pop++
-        else
-            notpop++
-    */
 
+    bool valid;
+    do 
+    {
+    if (response == "Y" || response == "y") {
+        // ask if they want to spend 1 oxygen to get 2 more meters
+        // or 2 oxygen to get 5
+        valid = true;
+    } else if (response == "N" || response == "n") {
+
+        cout << "Okay, word.\n";        
+        valid = true;
+    } else {
+        cout << "Invalid input, try again.\n";
+        cout << "Only input 'Y' or 'N'\n";
+        valid = false;
+    }   
+        
+    } while (!valid);
+
+}
 
 /*
     actions:
--- main.cpp
+++ main.cpp
@@ -5,27 +5,19 @@
 using namespace std;
 
 // global constants
-
-// function prototypes
-void intro();
-void scene1();
+bool playing = true;
+bool dead = false;
+bool won = false;
 
 // main function
 int main()
 {
-
     intro();
-    scene1();
+
+    battle();
+    
 
     return 0;
-
-    /*
-    while playing 
-        battle
-
-
-
-    */
 }
 
 void intro()
@@ -36,15 +28,4 @@ void intro()
     cout << "But while you were out your underwater lab exploded!\n";
     cout << "As the sole survivor it is up to you to reach the surface, battling sea monsters along the way";
     cout << "Just make sure you don't run out of oxygen...\n\n";
-}
-
-void scene1()
-{
-    cout << "'And that is why my beliefs are inherently correct and yours are wrong!'\n";
-    cout << "'That's rich,' Marcus snickered, 'The teacher may know some math, but that may be it'\n";
-    cout << "'What do you think?' ";
-    string thoughts;
-    cin >> thoughts;
-    cout << "\n'Uh huh,' Marcus replied, 'Well we better get to the next class'\n";
-    cout << "--AN IDIOT APPROACHES--\n";
 }
