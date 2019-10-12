4a87b94bbb9ee066597d7aebafcbef48fc195f61
hawkeyeshi <hawkeyeshi@gmail.com>
2019-10-12 12:05:30 -0400

    Added psuedo code

---
+++ battle.cpp
@@ -0,0 +1,40 @@
+/*
+    actions:
+        attacks:
+            harpoon
+                durabilityHarpoon--
+                healthOpponent--
+            punch
+                healthOpponent--
+                "insert punch line"
+            slap
+                instant KO
+                only after x turns
+
+        defenses:
+            bite
+                healthOpponent--
+                health++
+                chanceDeath++
+            block
+                blocks next move
+        
+    
+    bubbleScene:
+        bubble
+            is x1-x2 meters away
+            you can throw the tank z meters
+            upgrade for _ oxygen
+
+    goingUpTowardsSurface
+        distance per turn can be ugraded
+
+    currency:
+        oxygen:
+            goes down with each battle
+            can be used to level up
+                health
+                defense
+                attack
+
+*/
