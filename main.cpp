dbd1844276e97f5ce9bea5f7b7ed03c4cffcb46d
Megan Greensmith <megangreensmith@MacBook-Pro-6.local>
2019-10-12 18:21:44 -0400

    funky text

--- main.cpp
+++ main.cpp
@@ -30,6 +30,16 @@ int main()
 
 void intro()
 {
+
+    cout <<"  ___             ___            __                                        \n";
+    cout <<"  )  _  _)_ _     ) ( _   _     ) ) _   _   _                              \n";
+    cout <<"_(_ ) ) (_ (_)   (   ) ) )_)   /_/ )_) )_) )_)                             \n";
+    cout <<"                        (_        (_  (_  (                                \n";
+    cout <<" __         ___             _                                              \n";
+    cout <<" )_)         ) ( _   _     /_) ) _   _   _ ( _      _)_ ( _   _ _  o  _  _ \n";
+    cout <<"/__) (_(    (   ) ) )_)   / / ( (_( (_) )   ) ) (_( (_   ) ) ) ) ) ( (_ (  \n";
+    cout <<"       _)          (_             _)              _)                    _) \n\n\n";
+
     cout << "Welcome to Into The Deep, a game where your instincts and quick thinking determines whether you live or die.\n";
     cout << "You are part of an elite group of marine biologists, on a secret mission to discover a new type of oxygen-emitting algae.\n";
     cout << "This morning you headed down to the sea floor to collect more samples...\n";
